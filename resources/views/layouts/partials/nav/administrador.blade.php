4e6994145ae29b61edb31dfe4c36e76eda360e04
Rodrigo <rodrigoxj32@hotmail.com>
2017-04-05 06:16:05 -0600

    creacion de menu de medicamentos

--- resources/views/layouts/partials/nav/administrador.blade.php
+++ resources/views/layouts/partials/nav/administrador.blade.php
@@ -15,6 +15,16 @@
                         </ul>
                     </li>
                     <!-- Fin Menu Usuario -->
+
+                    <!-- Inicio Menu Medicamentos -->
+                    <li class="treeview">
+                        <a href="#"><i class='fa fa-link'></i> <span>Medicamentos</span> <i class="fa fa-angle-left pull-right"></i></a>
+                        <ul class="treeview-menu">
+                            <li><a href="{{route('medicamentos.index')}}">Ver medicamentos</a></li>
+                            <li><a href="{{route('medicamentos.create')}}">Ingresar medicamento</a></li>
+                        </ul>
+                    </li>
+                    <!-- Fin Menu Medicamentos -->
                 </ul>
             </li>
 <!-- FIN MENU ADMINISTRADOR -->
